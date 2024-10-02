32b61831c1142e90ec9888c98d6e6dbd892f42dc
Kevin Burk <xavierholt@gmail.com>
2024-10-01 20:11:29 -0700

    October 1 lecture notes.

---
+++ notes/2024-10-01b-control-flow.py
@@ -0,0 +1,51 @@
+mystr = "hello world"
+
+a = 12
+b = 47
+
+# a # doesn't print!
+# print(a) # definitely prints
+
+# if a % 2 == 0:
+#     print("a is even")
+#     print("kajhdkjashdkjashk")
+#     if True:
+#         print("hello")
+# else:
+#     print("a is odd")
+
+# if b % 2 == 0:
+#     print("b is even")
+#     if True:
+#         print("also hello")
+# else:
+#     print("b is odd")
+
+# if a > 100:
+#     print("a is big")
+# elif a < 5:
+#     print("a is small")
+# else:
+#     print("a is medium")
+
+# i = 0
+# while i < 5:
+#     print("i =", i)
+#     i += 1
+
+# for c in "cat":
+#     print(c)
+
+# for x in [1, 2, 3, 7]:
+#     print(x, end=' ')
+# print()
+
+# for i in range(0, 5):
+#     print(i)
+
+
+def foo(x):
+    print("fou(", x, ")")
+
+foo(17)
+foo(18)
